741e7efaed57a333d0a078d4ce90f1450d24200e
Lakiel Wade <lakielw@hotmail.com>
2018-10-11 15:36:43 -0400

    Initial commit

---
+++ app/src/main/java/com/cis470/lakiel/ocrreader/OcrDetectorProcessor.java
@@ -0,0 +1,49 @@
+package com.cis470.lakiel.ocrreader;
+
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+
+/**
+ * A very simple Processor which gets detected TextBlocks and adds them to the overlay
+ * as OcrGraphics.
+ */
+public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
+
+    private GraphicOverlay<OcrGraphic> mGraphicOverlay;
+
+    OcrDetectorProcessor(GraphicOverlay<OcrGraphic> ocrGraphicOverlay) {
+        mGraphicOverlay = ocrGraphicOverlay;
+    }
+
+    /**
+     * Called by the detector to deliver detection results.
+     * If your application called for it, this could be a place to check for
+     * equivalent detections by tracking TextBlocks that are similar in location and content from
+     * previous frames, or reduce noise by eliminating TextBlocks that have not persisted through
+     * multiple detections.
+     */
+    @Override
+    public void receiveDetections(Detector.Detections<TextBlock> detections) {
+        mGraphicOverlay.clear();
+        SparseArray<TextBlock> items = detections.getDetectedItems();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+            if (item != null && item.getValue() != null) {
+                Log.d("OcrDetectorProcessor", "Text detected! " + item.getValue());
+            }
+            OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
+            mGraphicOverlay.add(graphic);
+        }
+    }
+
+    /**
+     * Frees the resources associated with this detection processor.
+     */
+    @Override
+    public void release() {
+        mGraphicOverlay.clear();
+    }
+}
